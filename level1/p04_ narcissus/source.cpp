0592d627dd31188720548c7c1ebac25bb43577ad
Y-saber-icarus-black-lotus <1340319151@qq.com>
2021-03-03 22:42:58 +0800

    Diophantus_completed

--- level1/p04_ narcissus/source.cpp
+++ level1/p04_ narcissus/source.cpp
@@ -2,6 +2,14 @@
 #include<math.h>
 #include<stdio.h>
 using namespace std;
+bool judge(int n) {
+	bool ex = true;
+	if (n < 2)return false;
+	for (int i = 2; i <= sqrt(n); i++) {
+		if (n % i == 0) ex = false;
+	}
+	return ex; 
+}
 int main() {
   cout <<(5+4)/(1-1.0/6-1.0/12-1.0/7-1.0/2)-4 <<endl;
   system("pause");
