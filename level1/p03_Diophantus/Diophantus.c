cdc663a9f0fcabb2d4703f4b08af2ccdd60adb96
HuKou42 <79916298+HuKou42@users.noreply.github.com>
2021-03-03 22:49:21 +0800

    完成Diophantus

---
+++ level1/p03_Diophantus/Diophantus.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+  float age=2;
+  while ((1.0/6)*age+(1.0/12)*age+(1.0/7)*age+5+(1.0/2)*age+4-age>=0.01){
+    age++;
+  }
+
+  printf("When his son died Diophantus is %.0f years old\n",age-4 );
+
+  return 0;
+}
