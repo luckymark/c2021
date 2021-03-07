67767af750f230c0fae82339a208808d4b566e4e
frostylight <810560476@qq.com>
2021-03-07 17:21:20 +0800

    optimize p01_runningLetter

--- level1/p01_runningLetter/runningLetter.cpp
+++ level1/p01_runningLetter/runningLetter.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 #include<windows.h>
+#include<conio.h>
 using namespace std;
 inline void putspace(const int &t){
     for (int i = 0;i < t;++i)putchar(' ');
 }
-const int length = 100;
-const int cmdlength = 120;
-char letter[length];
+int length = 100, cmdlength;
+char letter[1000];
 inline bool isWhite(const char &c){
     return c == ' ' || c == '\n' || c == '\r' || c == EOF;
 }
@@ -23,20 +23,25 @@ inline void putLetter(){
         putchar(letter[i]);
 }
 int now = 0;
-bool dr = true;
+bool dr = true;//moving direction(true-right,false-left)
 int main(){
-    printf("input a letter or word\n");
+    HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO bInfo;
+    GetConsoleScreenBufferInfo(hOutput, &bInfo);
+    length = cmdlength = bInfo.dwSize.X - 1;//bInfo.dwSize.X(the width of cmd)
+    printf("input a letter or word:");
     getLetter();
+    printf("press any key to stop\n");
     while (true){
-        //system("cls");
-        putchar('\r');
+        putchar('\r');//use '\r' to get back to the start of line
         putspace(now);
         putLetter();
-        putchar(' ');
+        if (!dr)putchar(' ');//put a space to cover the character
         if (dr)++now;
         else --now;
-        if (now >= cmdlength || now <= 0)dr = !dr;
-        Sleep(30);
+        if (now >= cmdlength - (int)strlen(letter) - 1 || now <= 0)dr = !dr;//change direction
+        if (kbhit() && getch())break;//break if press a key
+        Sleep(30);//30ms may be smooth enough
     }
     return 0;
 }
