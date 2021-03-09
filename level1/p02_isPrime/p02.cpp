6aa1005c1e5bc3db9ddffcce0b94523adea83687
the-foolish <zoulycn@gmail.com>
2021-03-09 20:26:44 +0800

    finish p02

---
+++ level1/p02_isPrime/p02.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#define ll long long
+
+inline ll read(){
+	ll x=0,f=1;
+	char ch=getchar();
+	while(ch<'0'||ch>'9'){
+		if(ch=='-') 
+		f=-1;
+		ch=getchar();
+	}
+	while(ch>='0'&&ch<='9'){
+		x=10*x+ch-'0';
+		ch=getchar();
+	}
+	return x*f;
+}
+
+int main(){
+	ll n=read();
+	if(n<=0){
+		printf("The input is wrong.");
+	}
+	else if(n==1||(n%2==0&&n!=2)){
+		printf("%lld isn't prime.",n);
+	}
+	else{
+		for(ll i=3;i*i<n;i+=2){
+			if(n%i==0){
+				printf("%lld isn't prime.",n);
+				return 0;
+			}
+		}
+		printf("%lld is prime.",n);
+	}
+	return 0;
+}
