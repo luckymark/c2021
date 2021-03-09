34b07defbd2e3d0d562de39b3ccc21c0bd81400d
himcrh <1178990376@qq.com>
2021-03-09 20:03:23 +0800

    Prime succeed

---
+++ level1/p02_isPrime/1_prime.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+using namespace std;
+int main(){
+	int n;
+	scanf("%d",&n);
+	bool flag = true;
+	if(n==1){
+		printf("1 is not prime\n");
+		return 0;
+	}
+	for(int i=2;i*i<=n;i++){
+		if(!(n%i)){
+			flag = false;
+			break;
+		}
+	}
+	if(flag)
+		printf("%d is prime\n",n);
+	else
+		printf("%d is not prime\n",n);
+	return 0;
+}
