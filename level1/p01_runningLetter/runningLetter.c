45fac38ef58260f977fbd23b6d8496586f9fe340
Crazycat-tj <2321169422@qq.com>
2021-03-09 21:37:18 +0800

    wrong

--- level1/p01_runningLetter/runningLetter.c
+++ level1/p01_runningLetter/runningLetter.c
@@ -5,7 +5,7 @@
 void main() {
     int n = 0;
     const int right_border = 105, left_border = 0;
-    char value = 1;//value为1时向右移动，否则向左移动 
+    char value = 1;
     while (1) {
         if (value) {
             for (int i = 0; i < n; i++) {
@@ -13,7 +13,7 @@ void main() {
             }
             printf("Running Word");
             n++;
-            system("cls");//清屏 
+            system("cls");
             if (n == right_border) {
                 value = 0;
             }
