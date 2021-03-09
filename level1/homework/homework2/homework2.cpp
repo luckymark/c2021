d295db6131edf0cdaf62f8aa28bd56d1de091586
郭家瑞 <1433435802@qq.com>
2021-03-09 21:17:20 +0800

    Level1 作业2

---
+++ level1/homework/homework2/homework2.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int x = 0, i = 2;
+    bool suShu = true;
+    scanf("%d", &x);
+    if (x <= 1)
+    {
+        printf("不是素数");
+        return 0;
+    }
+    while (i <= x / 2)
+    {
+        if (x % i == 0)
+        {
+            suShu = false;
+            break;
+        }
+        i++;
+    }
+    if (suShu) printf("是素数");
+    else printf("不是素数");
+}
+
