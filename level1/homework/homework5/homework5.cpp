a10d4f8cc4ac1e84c5912b20c1e8c5605265a5b4
郭家瑞 <1433435802@qq.com>
2021-03-10 18:56:16 +0800

    2-1000的素数

---
+++ level1/homework/homework5/homework5.cpp
@@ -0,0 +1,30 @@
+﻿#include<stdio.h>
+#include<time.h>
+
+int main()
+{
+	clock_t start, finish;
+	start = clock();
+	printf("2\n");
+	int n = 3,i=3;
+	bool isPrime;
+	while (n <= 1000)
+	{
+		i = 3;
+		isPrime = 1;
+		while (i < n / 3)
+		{
+			if (n % i == 0)
+			{
+				isPrime = 0;
+				break;
+			}
+			i += 2;
+		}
+		if(isPrime)
+			printf("%d\n", n);
+		n += 2;
+	}
+	finish = clock();
+	printf("计算时间%f", (float)(finish - start) * 1000 / CLOCKS_PER_SEC);
+}
