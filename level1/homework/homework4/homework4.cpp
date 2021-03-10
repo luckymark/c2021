b7baae4806578dca3adfa368bec47827d37c1a06
郭家瑞 <1433435802@qq.com>
2021-03-10 17:15:41 +0800

    作业4 水仙花数

---
+++ level1/homework/homework4/homework4.cpp
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+#include<math.h>
+
+int main() 
+{
+	int n = 100;
+	while (n <= 999)
+	{
+		if (n == pow(n / 100, 3) + pow(n % 100 / 10, 3) + pow(n % 10, 3))
+			printf("%d\n", n);
+		n++;
+	}
+}
