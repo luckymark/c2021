0323d593c856b7acabbf73d526276281d290b551
NamelessOIer <70872016+NamelessOIer@users.noreply.github.com>
2021-03-16 22:32:12 +0800

    10秒内100万位

--- level2/PI/1.py
+++ level2/PI/1.py
@@ -1,31 +1,26 @@
 import gmpy2
+import time
+
+start = time.time()
 
 def sqr(x):
 	return x * x;
 
-I = 10 ** 200000
+N = 1000000
+
+I = 10 ** (2 * N)
 
 a = I
 b = gmpy2.iroot(2 * I * I, 2)[0] // 2
 t = I * I * I // 4
 p = I
 
-for i in range(25):
+for i in range(20):
 	(a, b, t, p)=((a + b) // 2, gmpy2.iroot(a * b, 2)[0],t - p * sqr((a - b) // 2),p * 2)
 
-PI = sqr(a + b) * I * I // (4 * t)
+PI = str(sqr(a + b) * I * I // (4 * t))
 
-f = []
-while(PI):
-	f.append(PI % 10)
-	PI //= 10
+end = time.time()
+print("Time=",end-start)
 
-f.reverse()
-
-print("3.",end="")
-for i in range(1,100001):
-	print(f[i],end="")
-
-print()
-
-
+print(PI[: 1] + '.' + PI[1: N])
