0915592d5885fe58ea1ab7195d90c363fefb7bd5
Hepper123 <1091720669@qq.com>
2021-03-22 21:07:46 +0800

    c

--- level1/p02_isPrime/isPrime.cpp
+++ level1/p02_isPrime/isPrime.cpp
@@ -1,31 +1,30 @@
 #include <stdio.h>
 
+bool isPrime(int num)
+{
+	if(num==1)
+	{
+		return false;
+	}
+	for(int i =2;i*i<=num;i++)
+	{
+		if(num%i==0)return false;
+	}
+	return true;
+}
+
+
 int main()
 {
 	int n = 0;
-	scanf_s("%d", &n);
-	int Isprime = 1;
-	for (int j = 2; j <= n / 2; j++)
+	scanf("%d", &n);
+	if (isPrime(n))
 	{
-		if (n % j  == 0)
-		{
-			Isprime = 0;
-			break;
-		}
-	}
-	if (n == 1)
-	{
-		printf("%d不是素数", n);
+		printf("%d是素数", n);
 	}
 	else
 	{
-		if (Isprime == 1)
-		{
-			printf("%d是素数", n);
-		}
-		if (Isprime == 0)
-		{
-			printf("%d不是素数", n);
-		}
+		printf("%d不是素数", n);
 	}
+
 }
