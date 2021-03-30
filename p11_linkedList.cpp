89343a51419911043aac5f4bea4e46046109eac8
zhouzijian2000 <79912596+zhouzijian2000@users.noreply.github.com>
2021-03-30 23:48:37 +0800

    Update p11_linkedList.cpp

--- p11_linkedList.cpp
+++ p11_linkedList.cpp
@@ -1,75 +1,156 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct linkedlist
+struct Node
 {
-	linkedlist*next;
 	int key;
+	Node*next;
 };
-void insert(linkedlist*h,linkedlist*p)
+void append(Node*head,Node*newnode)
 {
-	linkedlist*t=h;
+	Node*p;
+	p=head;
 	while(1)
 	{
-		if(t->next==NULL)
+		if(p->next==NULL)
 		{
-			t->next=p;
+			p->next=newnode;
 			break;
-		} 
-		t=t->next;
+		}
+		p=p->next;
 	}
 }
-void _delete(linkedlist*h)
+void _delete(int n,Node*head)
 {
-	h=h->next;
-}
-void search(linkedlist*h,int k)
-{
-	linkedlist*t=h;
-	for(int i=0;t!=NULL;i++)
+	Node*p,*q;
+	p=q=head;//不要忘记初始化； 
+	for(int i=1;i<n;i++)
 	{
-		if(t->key==k)	cout<<i<<" ";
-		t=t->next;
+		if(p==NULL)
+		{
+			cout<<"error"<<endl;
+			return;
+		}
+		p=p->next;
+		if(i<n-1) q=q->next;
 	}
-	cout<<-1<<endl;
+	q->next=p->next;
+	free(p);
 }
+void insert(int n,Node*head,Node*newone)//指插入在第n位的后面 ;
+{
+	Node*p=head;
+	for(int i=1;i<n;i++)
+	{
+		p=p->next;
+	}
+	newone->next=p->next;
+	p->next=newone;
+}
+Node*reverse(Node*head)
+{
+	Node*n,*p,*q;
+	n=head;
+	p=head->next;
+	if(p==NULL) return head;
+	q=p->next;
+	while(p!=NULL)
+	{
+		p->next=n;
+		n=p;
+		p=q;
+		if(q!=NULL) q=q->next; //不能让q“越界”； 
+	}
+	head->next=NULL;
+	return n;
+} 
 int main()
 {
-	linkedlist*head;
-	int key,n;
-	cin>>n>>key;
-	head=(linkedlist*)malloc(n*sizeof(linkedlist));
+	Node*head;
+	head=(Node*)malloc(sizeof(Node));
 	if(head==NULL)
 	{
-		cout<<"申请内存失败！"
+		cout<<"error"<<endl;
 		return 0;
 	}
-	*head=(linkedlist){NULL,key};
-	for(int i=1;i<n;i++)
+	
+	int v;
+	cin>>v;
+	*head=(Node){v,NULL};
+	while(1)
 	{
-		cin>>key;
-		head[i]=(linkedlist){NULL,key};
-		insert(head,head+i);
+		cin>>v;
+		if(v==-1) break;
+		Node*p;
+		p=(Node*)malloc(sizeof(Node));
+		if(p==NULL)
+		{
+			cout<<"error"<<endl;
+			return 0;
+		}
+		*p=(Node){v,NULL};
+		append(head,p);
 	}
-	linkedlist*t=head;
-	for(int i=0;i<n;i++)
+	
+	Node*p=head;
+    while(p!=NULL)
 	{
-		cout<<t->key<<" ";
-		t=t->next;
+		cout<<p->key<<" ";
+		p=p->next;
 	}
 	cout<<endl;
-	head->next=NULL;
-	for(int i=1;i<n;i++)
+	
+	head=reverse(head);
+	p=head;
+	while(p!=NULL)
 	{
-		head[i].next=head+i-1;
-	}
-	t=head+n-1;
-	for(int i=0;i<n;i++)
-	{
-		cout<<t->key<<" ";
-		t=t->next;
+		cout<<p->key<<" ";
+		p=p->next;
 	}
 	cout<<endl;
-	search(head+n-1,5);
-	free(head);
+	
+	p=head;
+	int flag=0;
+	for(int i=1;p!=NULL;i++)
+	{
+		if(p->key==5)
+		{
+			cout<<i<<endl;
+			flag=1;
+		}
+		p=p->next;
+	}
+	if(flag==0) cout<<-1<<endl;
+	
+/*用于测试_delete函数	
+	_delete(2,head);
+	p=head;
+	while(p!=NULL)
+	{
+		cout<<p->key<<endl;
+		p=p->next;
+	} */
+	
+/*用于测试insert函数 
+	Node*t;
+	int m;
+	t=(Node*)malloc(sizeof(Node));//不要忘了用malloc给t分配内存！ 
+	cin>>m>>v;
+	*t=(Node){v,NULL};
+	insert(m,head,t);
+	p=head;
+	while(p!=NULL)
+	{
+		cout<<p->key<<endl;
+		p=p->next;
+	} */
+	
+//接下来是释放所有用malloc请求的内存 
+	p=head;
+	while(p!=NULL)
+	{
+		Node*q=p->next;
+		free(p);
+		p=q;
+	}
 	return 0;
-} 
+}
