bc58d545bcbabc3d399b15dbf0ebe7ecd4c25f48
2020080908009 <79989394+2020080908009@users.noreply.github.com>
2021-04-05 16:24:28 +0800

    level 1 finish

---
+++ level1/p08_hanoi/hanoi.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+void hanoi(int n, char a, char b, char c);
+void move(char from, char to);
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+	hanoi(n, 'A', 'B', 'C');
+	return 0;
+}
+void hanoi(int n, char a, char b, char c)
+{
+	if (n == 1)
+	{
+		move(a, c);
+	}
+	else
+	{
+		hanoi(n - 1, a, c, b);
+		move(a, c);
+		hanoi(n - 1, b, a, c);
+		return;
+	}
+}
+void move(char from, char to)
+{
+	printf("%c->%c\n", from, to);
+	return;
+}
