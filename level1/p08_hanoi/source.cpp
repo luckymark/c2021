a465ec47b3771c5bf14dddb26cba34b091645200
xenoppy <79912817+xenoppy@users.noreply.github.com>
2021-04-13 10:16:21 +0800

    已修改

--- level1/p08_hanoi/source.cpp
+++ level1/p08_hanoi/source.cpp
@@ -4,23 +4,22 @@
 #include <Windows.h>
 #include <time.h>
 using namespace std;
-int saves[10000];
-int hanoi(int n)
+int hanoi(int n, int from, int goal, int temp)
 {
-  if (saves[n] == 0)
+  if (n != 1)
   {
-    saves[n] = 2 * hanoi(n - 1) + 1;
-    return 2 * hanoi(n - 1) + 1;
+    int a = hanoi(n - 1, from, temp, goal) + hanoi(1, from, goal, temp) + hanoi(n - 1, temp, goal, from) + 1;
+    return a;
   }
   else
-    return saves[n];
+    cout << (char)(from + 65) << "->" << (char)(goal + 65) << endl;
+  return 1;
 }
 int main()
 {
-  saves[1] = 1;
   int n;
   cin >> n;
-  cout << hanoi(n) << endl;
+  cout << hanoi(n, 0, 2, 1) << endl;
   system("pause");
   return 0;
 }
